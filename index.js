3269a245099f1e28926741d87b3f2580bbeab59b
John Syrinek <johntron@users.noreply.github.com>
2016-08-01 16:42:50 -0500

    Updated at hyperdev.com

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 var express = require('express');
 var app = express();
-var etag = '"6cbeebe0e3de380a47f72b8ecd18812b"';
+var etag = '"6cbeebe0e3de380a47f72b8ecd18812f"';
 
 app.use(express.static('public', {maxAge: 1000}));
-app.get('/test/1', function (req, res) {
+
+app.get('/test', function (req, res) {
   console.log(req.get('If-None-Match'), etag);
   
-  res.set({
-    'Cache-Control': 'max-age=0, private, must-revalidate',
-    'ETag': etag,
-	'Connection': 'keep-alive'
-  });
+  res.set('ETag', etag);
   
   if (req.get('If-None-Match') === etag) {
-    res.status(304).end();
-    return;
+    return res.status(304).end();
   }
   
-  res.set({
-	  'Transfer-Encoding': 'chunked',
-  	  'Content-Type': 'application/json; charset=utf-8'
-  });
   res.end('{"response": "success!"}');
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-
 listener.on('connection', function(socket) {
   console.log("A new connection was made by a client.");
-  socket.setTimeout(30 * 1000); 
-  // 30 second timeout. Change this as you see fit.
+  socket.setTimeout(30 * 1000);
 })
